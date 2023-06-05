2d55600194a3d03ea71b9095ab2144b9f5678ea5
kevin-sys <ksinasignargomez@unicesar.edu.co>
2023-06-04 20:41:14 -0500

    commit IAC and Deploy EC2

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server1" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance-${var.username}"
+  }
+}
+
+resource "aws_instance" "app_server2" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance-${var.username}"
+  }
+}
+
+variable "username" {
+  description = "Usuario de kevin Gómez Cantillo en AWS"
+  default     = "kevin-sys"
+}
